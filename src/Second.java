66c57c3129dedd060e767f31f540604d4618db43
Vladyslava Hniedova <153238906+ladislavochka@users.noreply.github.com>
2024-03-16 00:49:00 +0200

    Second.java

--- src/Second.java
+++ src/Second.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Second {
+import java.util.Scanner;
+public class Second
+{
+    public static void task2()
+    {
+        Scanner scanner = new Scanner(System.in);// Об'єкт для зчитування вводу користувача
+
+        System.out.print("Введіть розмірність масиву: ");
+        int Lenght = scanner.nextInt();// Зчитування розмірності масиву
+        int[] array = new int[Lenght];// Створення масиву
+
+        System.out.println("Введіть елементи масиву:");
+        for (int i = 0; i < Lenght; i++)// Цикл для заповнення користувачем масиву
+        {
+            System.out.print("Елемент " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();// Зчитування введеного значення елемента масиву
+        }
+
+        int WrongIndex = -1;// Змінна для індексу першого елемента, який порушив послідовність
+
+        for (int i = 0; i < Lenght - 1; i++)//  Цикл для перебирання елементів масиву
+        {
+            if ((array[i] >= 0 && array[i + 1] >= 0) || (array[i] < 0 && array[i + 1] < 0))// Умова для знаходження порушного елемента, якщо наступний елемент має той самий знак, що і поточний
+            {
+                WrongIndex = i + 1;// Збереження індексу
+                break;//Вихід з циклу
+            }
+        }
+
+        if (WrongIndex == -1) // Якщо цикл не порушився , що виводимо нуль
+        {
+            System.out.println("0");
+        }
+        else // В іншому випадку виводимо значення порушного елемента та його індекс
+        {
+            System.out.println("Перший порушений елемент: " + array[WrongIndex] + ", порядковий номер: " + (WrongIndex + 1));
+        }
+    }
 }
