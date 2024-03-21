630280471f6db9ffa54e7cec22704f60371b04c9
Hellsius <vlkolohat@gmail.com>
2024-03-21 20:50:37 +0200

    correct first task

--- src/First.java
+++ src/First.java
@@ -1,39 +1,59 @@
 import java.util.Scanner;
-public class First
-{
-    public static void task1()
-    {
-        Scanner scanner = new Scanner(System.in);// Об'єкт для зчитування вводу користувача
+
+public class First {
+    private int length;
+    private int[] array;
+
+    public First() {
+        Scanner scanner = new Scanner(System.in);  // Об'єкт для зчитування вводу користувача
+
 
         System.out.print("Введіть розмірність масиву: ");
-        int lenght = scanner.nextInt();// Зчитування введеної розмірності масиву
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.println("спробуй ще");
+            System.out.print("Введіть розмірність масиву: ");
+        }
+        length = scanner.nextInt();  // Зчитування введеної розмірності масиву
 
-        int[] array = new int[lenght];// Створєння одновимірного масиву
+        array = new int[length];  // Створєння одновимірного масиву
 
-        for (int i = 0; i < lenght; i++)// Цикл для заповнення значеннями масиву користувачем
+        for (int i = 0; i < length; i++)  // Цикл для заповнення значеннями масиву користувачем
         {
             System.out.print("Введіть значення" + (i + 1) + "-го елемента:");
+            while (!scanner.hasNextInt()) {
+                scanner.nextLine();
+                System.out.println("спробуй ще");
+                System.out.print("Введіть розмірність масиву: ");
+            }
             array[i] = scanner.nextInt();
         }
+    }
 
-        System.out.println(" ");// Порожній рядок для розділення парних та непарних чисел
-
-        for (int i = 0; i < lenght; i++)// Цикл для перебирання всіх елементів у порядку зростання індексів
+    private void printEvenNumbers(){
+        for (int i = 0; i < length; i++)// Цикл для перебирання всіх елементів у порядку зростання індексів
         {
             if (array[i] != 0 && array[i] % 2 == 0)// Перевірка чи елемент не дорівнює нулю та чи є воно парним
             {
                 System.out.println("Парне число:" + array[i] + "(індекс" + (i + 1) + ")");// Виведення парних чисел
             }
         }
+    }
 
-        System.out.println(" ");// Порожній рядок для розділення парних та непарних чисел
-
-        for (int i = lenght - 1; i >= 0; i--)// Цикл для перебиранняя непарних чисел, окрів нулю, в порядку зменьшення їх індексу
+    private void printOddNumbers(){
+        for (int i = length - 1; i >= 0; i--)// Цикл для перебиранняя непарних чисел, окрів нулю, в порядку зменьшення їх індексу
         {
             if (array[i] != 0 && array[i] % 2 != 0)// Перевірка чи не дорівнює елемент нулю та чи є воно непарним
             {
                 System.out.println("Непарне число:" + array[i] + "(індекс" + (i + 1) + ")");// Виведення непарних чисел
             }
         }
+    }
+
+    public void runTask() {
+        System.out.println(" ");// Порожній рядок для розділення парних та непарних чисел
+        printEvenNumbers();
+        System.out.println(" ");// Порожній рядок для розділення парних та непарних чисел
+        printOddNumbers();
     }
 }
